e90afce35a425e497455d4026e19ead0823e58bd
webdeveloper510 <webdeveloper510@gmail.com>
2023-09-06 11:04:51 +0530

    database

--- health/settings.py
+++ health/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["54.84.161.43"]
 
 
 # Application definition
